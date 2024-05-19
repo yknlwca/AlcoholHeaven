7edce0319d7b3c3a687ab69f464108ff66c2d854
KWANGLIM <choikl3115@naver.com>
2024-05-19 16:18:17 +0900

    파일업로드

--- alcohol-project/src/components/food/FoodCreate.vue
+++ alcohol-project/src/components/food/FoodCreate.vue
@@ -3,64 +3,26 @@
   <div class="container" style="width: 70%">
     <div class="mb-3">
       <b class="form-label">제목</b>
-      <input
-        type="text"
-        class="form-control"
-        placeholder="제목을 입력해주세요."
-        v-model="newFood.title"
-      />
+      <input type="text" class="form-control" placeholder="제목을 입력해주세요." v-model="newFood.title" />
     </div>
     <div class="mb-3">
       <b class="form-label">작성자</b>
-      <input
-        style="width: 50%"
-        type="text"
-        class="form-control"
-        :value="loginUser.id"
-        readonly
-        />
+      <input style="width: 50%" type="text" class="form-control" :value="loginUser.id" readonly />
     </div>
     <div class="mb-3">
       <b class="form-label">세부 사항</b>
-      <input
-        style="width: 30%"
-        type="text"
-        class="form-control"
-        placeholder="메뉴이름"
-        v-model="newFood.menu"
-      /><input
-        style="width: 30%"
-        type="text"
-        class="form-control my-2"
-        placeholder="지역(ex.강남구)"
-        v-model="newFood.region"
-      /><input
-        style="width: 30%"
-        type="text"
-        class="form-control my-2"
-        placeholder="어울리는 술"
-        v-model="newFood.kindOf"
-      />
+      <input style="width: 30%" type="text" class="form-control" placeholder="메뉴이름" v-model="newFood.menu" /><input
+        style="width: 30%" type="text" class="form-control my-2" placeholder="지역(ex.강남구)"
+        v-model="newFood.region" /><input style="width: 30%" type="text" class="form-control my-2" placeholder="어울리는 술"
+        v-model="newFood.kindOf" />
     </div>
     <div class="mb-3">
       <b for="exampleFormControlTextarea1" class="form-label">안주 소개</b>
-      <textarea
-        class="form-control"
-        id="exampleFormControlTextarea1"
-        rows="3"
-        placeholder="나만의 안주를 소개해주세요."
-        v-model="newFood.content"
-      ></textarea>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="나만의 안주를 소개해주세요."
+        v-model="newFood.content"></textarea>
       <div class="mb-3 my-2">
-        <b for="formFileMultiple" class="form-label"
-          >안주 사진</b
-        >
-        <input
-          class="form-control"
-          type="file"
-          id="formFileMultiple"
-          multiple
-        />
+        <b for="formFileMultiple" class="form-label">안주 사진</b>
+        <input class="form-control" type="file" id="formFileMultiple" @change="handleFileUpload" />
       </div>
     </div>
     <button type="button" class="btn btn-outline-success" @click="createFood">
@@ -81,10 +43,24 @@ const newFood = ref({
   title: "",
   region: "",
   kindOf: "",
-  img: "null",
 });
+
+const selectedFile = ref(null);
+
+const handleFileUpload = (event) => {
+  selectedFile.value = event.target.files[0];
+};
+
 const createFood = function () {
-  store.createFood(newFood.value);
+  const formData = new FormData();
+  formData.append('file', selectedFile.value);
+  formData.append('userId', newFood.value.userId);
+  formData.append('menu', newFood.value.menu);
+  formData.append('content', newFood.value.content);
+  formData.append('title', newFood.value.title);
+  formData.append('region', newFood.value.region);
+  formData.append('kindOf', newFood.value.kindOf);
+  store.createFood(formData);
 };
 </script>
 
--- alcohol-project/src/components/friend/FriendCreate.vue
+++ alcohol-project/src/components/friend/FriendCreate.vue
@@ -3,56 +3,25 @@
   <div class="container" style="width: 70%">
     <div class="mb-3">
       <b class="form-label">제목</b>
-      <input
-        type="text"
-        class="form-control"
-        placeholder="제목을 입력해주세요."
-        v-model="newFriend.title"
-      />
+      <input type="text" class="form-control" placeholder="제목을 입력해주세요." v-model="newFriend.title" />
     </div>
     <div class="mb-3">
       <b class="form-label">작성자</b>
-      <input
-        style="width: 50%"
-        type="text"
-        class="form-control"
-        :value="loginUser.id"
-        readonly
-      />
+      <input style="width: 50%" type="text" class="form-control" :value="loginUser.id" readonly />
     </div>
     <div class="mb-3">
       <b class="form-label">세부 사항</b>
-      <input
-        style="width: 30%"
-        type="text"
-        class="form-control my-2"
-        placeholder="지역(ex.강남구)"
-        v-model="newFriend.region"
-      /><input
-        style="width: 30%"
-        type="text"
-        class="form-control my-2"
-        placeholder="좋아하는 술"
-        v-model="newFriend.kindOf"
-      />
+      <input style="width: 30%" type="text" class="form-control my-2" placeholder="지역(ex.강남구)"
+        v-model="newFriend.region" /><input style="width: 30%" type="text" class="form-control my-2"
+        placeholder="좋아하는 술" v-model="newFriend.kindOf" />
     </div>
     <div class="mb-3">
       <b for="exampleFormControlTextarea1" class="form-label">자기 소개</b>
-      <textarea
-        class="form-control"
-        id="exampleFormControlTextarea1"
-        rows="3"
-        placeholder="친구들에게 간단하게 소개해주세요."
-        v-model="newFriend.intro"
-      ></textarea>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="친구들에게 간단하게 소개해주세요."
+        v-model="newFriend.intro"></textarea>
       <div class="mb-3 my-2">
         <b for="formFileMultiple" class="form-label">본인을 어필할 사진을 올려주세요</b>
-        <input
-          class="form-control"
-          type="file"
-          id="formFileMultiple"
-          multiple
-        />
+        <input class="form-control" type="file" id="formFileMultiple" @change="handleFileUpload" />
       </div>
     </div>
     <button type="button" class="btn btn-outline-success" @click="createFriend">
@@ -73,10 +42,23 @@ const newFriend = ref({
   intro: "",
   region: "",
   kindOf: "",
-  img: "null",
 });
+
+const selectedFile = ref(null);
+
+const handleFileUpload = (event) => {
+  selectedFile.value = event.target.files[0];
+};
+
 const createFriend = function () {
-  store.createFriend(newFriend.value);
+  const formData = new FormData();
+  formData.append('file', selectedFile.value);
+  formData.append('userId', newFriend.value.userId);
+  formData.append('title', newFriend.value.title);
+  formData.append('intro', newFriend.value.intro)
+  formData.append('region', newFriend.value.region);
+  formData.append('kindOf', newFriend.value.kindOf);
+  store.createFriend(formData);
 };
 
 </script>
