6a35b447b2de27a88a461720015bba714bb51fee
klc <choikl3115@naver.com>
2024-05-22 09:35:03 +0900

    tt

--- alcohol-project/src/components/Home/Home.vue
+++ alcohol-project/src/components/Home/Home.vue
@@ -1,26 +1,24 @@
 <template>
-    <div class="container" v-if="store.signIn" style="width: 80%;">
-        <h1>외로운 사람들 모여라!</h1>
-        <imgSlide/>
-        <h4>실시간 순위!</h4>
-        <div class="d-flex justify-content-between">
-            <AlcoholRanking style="width: 30%;"/>
-            <FoodRanking style="width: 30%;"/>
-            <FriendRanking style="width: 30%;"/>
-        </div>
+  <div class="container" v-if="store.signIn" style="width: 80%">
+    <h1>외로운 사람들 모여라!</h1>
+    <imgSlide />
+    <h4>실시간 순위!</h4>
+    <div class="d-flex justify-content-between">
+      <AlcoholRanking style="width: 30%" />
+      <FoodRanking style="width: 30%" />
+      <FriendRanking style="width: 30%" />
     </div>
+  </div>
 </template>
 
 <script setup>
-import imgSlide from '@/components/Home/imgSlide.vue'
-import AlcoholRanking from '@/components/alcohol/AlcoholRanking.vue';
-import FoodRanking from '@/components/food/FoodRanking.vue';
-import FriendRanking from '@/components/friend/FriendRanking.vue';
-import { useUserstore } from '@/stores/user';
+import imgSlide from "@/components/Home/imgSlide.vue";
+import AlcoholRanking from "@/components/alcohol/AlcoholRanking.vue";
+import FoodRanking from "@/components/food/FoodRanking.vue";
+import FriendRanking from "@/components/friend/FriendRanking.vue";
+import { useUserstore } from "@/stores/user";
 
-    const store = useUserstore()
+const store = useUserstore();
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
