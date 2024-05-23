75c5000d0740896668fad4638676d9628ca8e8d0
KIM19684 <kht19684@gmail.com>
2024-05-23 10:16:26 +0900

    진짜 끝

--- alcohol-project/src/components/common/FooterView.vue
+++ alcohol-project/src/components/common/FooterView.vue
@@ -1,23 +1,27 @@
 <template>
   <div>
     <footer>
+      <hr />
       <div class="footer-container">
         <div class="footer-logo">
           <img src="@/assets/img/알코올천국로고_최종.png" alt="Company Logo" />
         </div>
         <div class="footer-links">
-          <h4>주요 링크</h4>
+          <h4>Links</h4>
           <ul>
-            <li><a href="#about">About Us</a></li>
-            <li><a href="#products">Products</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#faq">FAQ</a></li>
+            <a href="#about">About Us</a
+            ><br />
+            <a href="#contact">Contact</a
+            ><br />
+            <a href="#faq">FAQ</a>
           </ul>
         </div>
         <div class="footer-social">
           <h4>Follow Us</h4>
           <a href="https://facebook.com" target="_blank">Facebook</a>
+          <br />
           <a href="https://twitter.com" target="_blank">Twitter</a>
+          <br />
           <a href="https://instagram.com" target="_blank">Instagram</a>
         </div>
         <div class="footer-contact">
@@ -37,8 +41,9 @@
 
 <style scoped>
 footer {
-  background-color: #01AD6F;
-  color: white;
+  /* background-color: #01ad6f; */
+  background-color: white;
+  color: black;
   padding: 20px 0;
 }
 
@@ -55,27 +60,36 @@ footer {
   max-width: 150px;
 }
 
-.footer-links, .footer-social, .footer-contact {
+.footer-links,
+.footer-social,
+.footer-contact {
   flex: 1 1 200px;
   margin: 10px;
 }
 
-.footer-links ul, .footer-social a {
+.footer-links ul,
+.footer-social a {
   list-style: none;
   padding: 0;
   margin: 0;
 }
 
-.footer-links ul li, .footer-social a {
+.footer-links ul li,
+.footer-social a {
   margin: 5px 0;
 }
 
-.footer-links a, .footer-social a {
-  color: white;
+.footer-links a,
+.footer-social a {
+  /* color: white; */
+
+  color: black;
+
   text-decoration: none;
 }
 
-.footer-links a:hover, .footer-social a:hover {
+.footer-links a:hover,
+.footer-social a:hover {
   text-decoration: underline;
 }
 
